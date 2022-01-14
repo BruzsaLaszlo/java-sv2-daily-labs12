616c6ed8a7a437989acdf42b5b1533356b8206ca
Bruzsa László <lacika007@gmail.com>
2022-01-14 16:19:01 +0100

    day05

--- src/test/java/day05/StringStatisticsTest.java
+++ src/test/java/day05/StringStatisticsTest.java
@@ -2,34 +2,49 @@ package day05;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StringStatisticsTest {
 
-    String text = """
+    final String text = """
             Készíts egy StringStatistics nevű osztályt és benne,
             egy metódust aminek bementi paramétere egy szöveg és
             visszatérési értéke egy adatszerkezet, amiből kiolvasható,
             hogy melyik karakterből hány darab szerepel benne!
             """;
 
-    StringStatistics stat = new StringStatistics();
-
     @Test
     void getCharactersStatistics() {
 
-        assertEquals(1, stat.getCharactersStatistics(text).get('ű'));
-        assertEquals(2, stat.getCharactersStatistics(text).get('ő'));
-        assertEquals(4, stat.getCharactersStatistics(text).get('\n'));
+        var characterStat = new StringStatistics()
+                .getCharactersStatistics(text);
+
+        printMap(characterStat);
+
+        assertEquals(1, characterStat.get('ű'));
+        assertEquals(2, characterStat.get('ő'));
+        assertEquals(4, characterStat.get('\n'));
 
     }
 
     @Test
     void getCharactersStatisticsWithStream() {
 
-        assertEquals(1, stat.getCharactersStatisticsWithStream(text).get('ű'));
-        assertEquals(2, stat.getCharactersStatisticsWithStream(text).get('ő'));
-        assertEquals(4, stat.getCharactersStatisticsWithStream(text).get('\n'));
+        var characterStat = new StringStatistics()
+                .getCharactersStatisticsWithStream(text);
+
+        printMap(characterStat);
+
+        assertEquals(1, characterStat.get('ű'));
+        assertEquals(2, characterStat.get('ő'));
+        assertEquals(4, characterStat.get('\n'));
 
     }
+
+    void printMap(Map<Character,Integer> map) {
+        map.entrySet().forEach(System.out::println);
+    }
+
 }
